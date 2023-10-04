b773d88b5e0132a60655580783c69d7fcbb2bae5
OREN23 <143612873+OREN23@users.noreply.github.com>
2023-10-04 13:40:32 +0100

    20/09/2023

---
+++ src/MATHS123.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class MATHS123 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int score = 0;
+        int lives = 3;
+
+        System.out.println("Welcome to the Math Quiz Game!");
+
+        for (int i = 1; i <= 10; i++) {
+            if (lives == 0) {
+                System.out.println("You've run out of lives. Quiz ends!");
+                break;
+            }
+
+            int num1 = (int) (Math.random() * 10);
+            int num2 = (int) (Math.random() * 10);
+
+            // Generate a random operator (+, -, *)
+            String[] operators = { "+", "-", "*" };
+            int operatorIndex = (int) (Math.random() * operators.length);
+            String operator = operators[operatorIndex];
+
+            System.out.print("Question " + i + ": What is " + num1 + " " + operator + " " + num2 + "? ");
+            int userAnswer = scanner.nextInt();
+
+            int correctAnswer;
+            switch (operator) {
+                case "+":
+                    correctAnswer = num1 + num2;
+                    break;
+                case "-":
+                    correctAnswer = num1 - num2;
+                    break;
+                case "*":
+                    correctAnswer = num1 * num2;
+                    break;
+                default:
+                    correctAnswer = 0;
+            }
+
+            if (userAnswer == correctAnswer) {
+                System.out.println("Correct!");
+                score++;
+            } else {
+                System.out.println("Incorrect. The correct answer is " + correctAnswer);
+                lives--; // Decrease the number of lives on incorrect answer
+            }
+        }
+
+        if (lives > 0) {
+            System.out.println("Quiz completed! Your score is: " + score + "/10");
+            System.out.println("Well done!");
+        } else {
+            System.out.println("Quiz completed! Your score is: " + score + "/10");
+        }
+
+        scanner.close();
+    }
+}
